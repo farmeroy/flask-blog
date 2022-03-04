8c5eef955e908c480793dc318dd6f3bd986fe54e
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-04 17:47:08 +0100

    added auth decorator to validate user before loading view

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -56,6 +56,8 @@ def login():
             error = 'Incorrect password.'
 
         if error is None:
+            # session is a dict that stores data accross requests
+            # the data is stored in a cookie sent to the browser
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('index'))
@@ -63,6 +65,39 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+# this decorator calls the function before the view is calls
+# no matter what the URL request is
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+                'SELECT * FROM user WHERE id = ?', (user_id,)
+                ).fetchone()
+
+# A decorator to check if a user is logged in
+# before allowing the view to load
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+
 
 
 
