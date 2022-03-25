775a8fd56615e58c47eb3942b465fea646eaa588
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-25 01:13:02 +0100

    Fixed regester->register, data= typos. Put doubled quotes around the href string

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -4,7 +4,7 @@ from flaskr.db import get_db
 def test_index(client, auth):
     response = client.get('/')
     assert b"Log In" in response.data
-    assert b"Regester" in response.data
+    assert b"Register" in response.data
     
     auth.login()
     response = client.get('/')
@@ -12,7 +12,7 @@ def test_index(client, auth):
     assert b"test title" in response.data
     assert b'by test on 2018-01-01' in response.data
     assert b"test\nbody" in response.data
-    assert b"href='/1/update'" in response.data
+    assert b'href="/1/update"' in response.data
 
 @pytest.mark.parametrize('path', (
     '/create',
@@ -48,7 +48,7 @@ def test_exists_required(client, auth, path):
 def test_create(client, auth, app):
     auth.login()
     assert client.get('/create').status_code == 200
-    client.post('/create', data{'title': 'created', 'body': ''})
+    client.post('/create', data={'title': 'created', 'body': ''})
 
     with app.app_context():
         db = get_db()
