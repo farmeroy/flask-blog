2d21573d2c2696d144cc1cc00cf60def03c7fad4
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-25 01:24:00 +0100

    removed empty space

--- flaskr/db.py
+++ flaskr/db.py
@@ -25,7 +25,6 @@ def close_db(e=None):
 
 def init_db():
     db = get_db()
-    
     # open_resource() opens a file relative to flaskr
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
